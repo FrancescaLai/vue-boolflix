0885868394e27761fc82b8cb4c6b18d5a75f9920
FrancescaLai <francescalai1690@gmail.com>
2021-04-09 16:29:20 +0200

    Js started (v-for not working)

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+  el: "#root",
+  data: {
+    movies: [],
+    searchMovie: ""
+  },
+  methods: {
+    search: function(){
+      axios.get('https://api.themoviedb.org/3/search/movie',{
+        params: {
+          api_key: "63adf4dc8b14837feb63c3cb75abb457",
+          query: this.searchMovie,
+          page: 1,
+          include_adult: false,
+          language: "en-US",
+        }
+      })
+      .then((risposta) =>{
+        this.movies = risposta.data.response;
+      }
+    }
+  }
+});
